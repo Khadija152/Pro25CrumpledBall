0913acbf7c5c1388a11c7c70c58cc25db489919d
Khadija152 <78963126+Khadija152@users.noreply.github.com>
2021-06-15 15:47:23 +0530

    Pro25CrumpledBall
    Pro25CrumpledBall

---
+++ sketch.js
@@ -0,0 +1,47 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+
+var dustbinObj,groundObject,paperObject;
+var engine,world;
+var dustBin,dustBinImg;
+
+function preload(){
+	dustBinImg = loadImage("dustbingreen.png");
+}
+
+function setup() {
+	createCanvas(1600,700);
+
+	dustBin = createSprite(1200,545,200,213);
+	dustBin.addImage(dustBinImg);
+	dustBin.scale = 0.65;
+
+	engine = Engine.create();
+	world = engine.world;
+	
+	groundObject = new ground(width/2,670,width,20);
+	dustbinObj = new dustbin(1200,650);
+	paperObject = new Paper(250,665,40,40);
+}
+
+function draw() {
+  background(230);
+
+  Engine.update(engine);
+ 
+  groundObject.display();
+  dustbinObj.display();
+  paperObject.display();
+
+  drawSprites();
+}
+
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
+		Matter.Body.applyForce(paperObject.body, paperObject.body.position,{x:150,y:-100});
+   }
+}
